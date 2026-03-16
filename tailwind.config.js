9c7e027e7407dedfa0579dd39376e55fd9b90ec3
WAIA-io <joel.decena@gmail.com>
2026-03-16 14:36:11 +0400

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,11 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2868
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red109\green115\blue120;\red23\green24\blue24;\red202\green202\blue202;
-\red183\green111\blue247;\red212\green212\blue212;\red113\green192\blue131;}
-{\*\expandedcolortbl;;\cssrgb\c50196\c52549\c54510;\cssrgb\c11765\c12157\c12549;\cssrgb\c83137\c83137\c83137;
-\cssrgb\c77255\c54118\c97647;\cssrgb\c86275\c86275\c86275;\cssrgb\c50588\c78824\c58431;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
-
-\f0\fs28 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 /** @type \{import('tailwindcss').Config\} */\cf4 \cb1 \strokec4 \
-\cf5 \cb3 \strokec5 export\cf4 \strokec4  \cf5 \strokec5 default\cf4 \strokec4  \cf6 \strokec6 \{\cf4 \cb1 \strokec4 \
-\cb3   content\cf6 \strokec6 :\cf4 \strokec4  \cf6 \strokec6 [\cf4 \cb1 \strokec4 \
-\cb3     \cf7 \strokec7 "./index.html"\cf6 \strokec6 ,\cf4 \cb1 \strokec4 \
-\cb3     \cf7 \strokec7 "./src/**/*.\{js,ts,jsx,tsx\}"\cf6 \strokec6 ,\cf4 \cb1 \strokec4 \
-\cb3   \cf6 \strokec6 ],\cf4 \cb1 \strokec4 \
-\cb3   theme\cf6 \strokec6 :\cf4 \strokec4  \cf6 \strokec6 \{\cf4 \cb1 \strokec4 \
-\cb3     extend\cf6 \strokec6 :\cf4 \strokec4  \cf6 \strokec6 \{\},\cf4 \cb1 \strokec4 \
-\cb3   \cf6 \strokec6 \},\cf4 \cb1 \strokec4 \
-\cb3   plugins\cf6 \strokec6 :\cf4 \strokec4  \cf6 \strokec6 [],\cf4 \cb1 \strokec4 \
-\cf6 \cb3 \strokec6 \}\cf4 \cb1 \strokec4 \
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
